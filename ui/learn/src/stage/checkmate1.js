d9967ced761f5c99ac8db32a8797947feca9ffd8
justremember <nothingpersonal.qwerty@gmail.com>
2021-07-01 18:57:08 +0800

    add translation strings for mate in one

--- ui/learn/src/stage/checkmate1.js
+++ ui/learn/src/stage/checkmate1.js
@@ -14,11 +14,11 @@ module.exports = {
   illustration: util.roundSvg(imgUrl),
   levels: [
     {
-      goal: 'attackYourOpponentsKing',
+      goal: 'toWinInShogi',
       fen: '6k2/9/6P2/9/9/9/9/9/9 b G2r2b3g4s4n4l17p 1',
     },
     {
-      goal: 'attackYourOpponentsKing',
+      goal: 'dropsCommonDeliverMate',
       fen: '7k1/9/7S1/9/9/9/9/9/9 b S2r2b4g2s4n4l18p 1',
     },
     {
@@ -46,20 +46,20 @@ module.exports = {
       fen: '4R3l/6g1k/5gsp1/6p1P/9/9/9/9/8L b r2b2g3s4n2l15p 1',
     },
     {
-      goal: 'attackYourOpponentsKing',
+      goal: 'chooseYourPieceCarefully',
       fen: '8l/6S1k/9/9/8g/9/9/9/9 b GS2r2b2g2s4n3l18p 1',
     },
     {
-      goal: 'attackYourOpponentsKing',
+      goal: 'chooseYourPieceCarefully',
       fen: '8l/7k1/7pb/7N1/9/9/9/9/9 b RGrb3g4s3n3l17p 1',
     },
     {
-      goal: 'attackYourOpponentsKing',
+      goal: 'chooseYourPieceCarefully',
       fen: '8l/6+Rgk/7pp/9/9/9/9/9/9 b GSr2b2g3s4n3l16p 1',
     },
     {
       // pawn
-      goal: 'attackYourOpponentsKing',
+      goal: 'mateWithADroppedPawnIs',
       fen: '7+P1/8g/7pk/9/7Sp/9/9/9/9 b LP2r2b3g3s4n3l14p 1',
       scenario: [
         {
@@ -71,7 +71,7 @@ module.exports = {
     },
     {
       // pawn 2
-      goal: 'attackYourOpponentsKing',
+      goal: 'mateWithAPushedPawnIs',
       fen: '7+P1/8g/7pk/9/7SP/9/9/9/9 b 2r2b3g3s4n4l15p 1',
     },
     {
@@ -92,7 +92,6 @@ module.exports = {
       goal: 'attackYourOpponentsKing',
       fen: '2R3gkl/3R3s1/7p1/7Np/9/9/9/9/9 b 2b3g3s3n3l16p 1',
     },
-
 
   ].map(function (l, i) {
     l.nbMoves = 1;
